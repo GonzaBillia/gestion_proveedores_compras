968b3a7a95ed937d2a37eb949d09e308c88c005d
GonzaBillia <gonzalobillia@gmail.com>
2024-12-31 10:39:52 -0300

    creo reporte de proveedor

--- libs/comparator/comparator.py
+++ libs/comparator/comparator.py
@@ -1,7 +1,7 @@
 from libs.comparator.controllers.process_controller import make_comparation, make_provider_comparation, setup_report
 from libs.comparator.services.reports.reports import make_report
 
-id_provider = 18
+id_provider = 28
 
 def comparate():
     unmatched, matches, unmatched_cb = make_comparation()
---
+++ libs/comparator/services/reports/data_frames/provider_report.py
@@ -0,0 +1,40 @@
+import pandas as pd
+
+def format_provider_report(df):
+    """
+    Selecciona las columnas especificadas de un DataFrame y retorna el DataFrame filtrado.
+    
+    Args:
+        df (pd.DataFrame): El DataFrame original.
+
+    Returns:
+        pd.DataFrame: El DataFrame filtrado con las columnas especificadas.
+    """
+    # Columnas que deseas seleccionar
+    columnas = ['idproducto', 'ean_x', 'descripcion_x', 'proveedor_x', 'laboratorio_x']
+
+    # Filtrar el DataFrame con las columnas especificadas
+    df_formateado = df[columnas].copy()
+
+    # Ordenar por 'laboratorio_x'
+    df = df_formateado.sort_values(by='laboratorio_x')
+
+    # Crear una lista para almacenar las filas con los saltos
+    result_rows = []
+    previous_value = None
+
+    # Iterar sobre las filas del DataFrame
+    for _, row in df.iterrows():
+        # Si el valor cambia, agregar una fila vacía
+        if previous_value is not None and row['laboratorio_x'] != previous_value:
+            result_rows.append({col: None for col in df.columns})
+        
+        # Agregar la fila actual
+        result_rows.append(row.to_dict())
+        previous_value = row['laboratorio_x']
+
+    # Crear el DataFrame final con los saltos
+    df_result = pd.DataFrame(result_rows)
+
+    # Retornar únicamente el DataFrame formateado
+    return df_result
